7463b887639ce12d983621f059921f45e56127b2
Harpine <aline.brunner@epfl.ch>
2021-12-09 22:30:34 +0100

    to tag version of multi logistic reg with tuned lasso l1

--- multi_logistic_reg.jl
+++ multi_logistic_reg.jl
@@ -35,50 +35,50 @@ write_csv("multi_logistic_reg_not_reg.csv", prediction_mult_logistic_not_reg_df)
 
 
 
-# model_mult_logistic_reg_l1 = LogisticClassifier(penalty = :l1, solver = ISTA(tol = 5e-4)) # solver to avoid warning : Proximal GD did not converge in 1000 iterations.
+model_mult_logistic_reg_l1 = LogisticClassifier(penalty = :l1, solver = ISTA(tol = 5e-4)) # solver to avoid warning : Proximal GD did not converge in 1000 iterations.
 
-# tuned_model_mult_logistic_reg_l1 = TunedModel(model = model_mult_logistic_reg_l1,
-#                                    resampling = CV(nfolds = 10),
-#                                    tuning = Grid(),
-#                                    range = range(model_mult_logistic_reg_l1, :lambda, lower = 6 , upper = 8, scale = :log),
-#                                    measure = auc)
-
-                              
-# mach_mult_logistic_reg_d_l1 = fit!(machine(tuned_model_mult_logistic_reg_l1, training_dropped_x_std, training_dropped_y))
-# pred_mult_logistic_reg_d_l1 = predict_mode(mach_mult_logistic_reg_d_l1, training_dropped_x_std)
-# err_rate_mult_logistic_reg_d_l1 = mean(pred_mult_logistic_reg_d_l1 .!= training_dropped_y)
-
-# print("lambda : ", report(mach_mult_logistic_reg_d_l1).best_history_entry.model.lambda, "\n")
-# print("l1 : ", err_rate_mult_logistic_reg_d_l1, "\n")
-
-# proba_mult_logistic_reg_d_l1 = broadcast(pdf, predict(mach_mult_logistic_reg_d_l1, test_data_std), true)
-# prediction_mult_logistic_reg_d_l1_df = DataFrame(id = 1:nrow(test_data_std), precipitation_nextday = proba_mult_logistic_reg_d_l1)
-# write_csv("multi_logistic_reg_tuned_l1.csv", prediction_mult_logistic_reg_d_l1_df)
-
-
-
-model_mult_logistic_reg_l2 = LogisticClassifier(penalty = :l2)
-
-tuned_model_mult_logistic_reg_l2 = TunedModel(model = model_mult_logistic_reg_l2,
+tuned_model_mult_logistic_reg_l1 = TunedModel(model = model_mult_logistic_reg_l1,
                                    resampling = CV(nfolds = 10),
                                    tuning = Grid(),
-                                   #range = range(model_mult_logistic_reg_l2, :lambda, lower = 3e-2, upper = 3e2, scale = :log),
-                                   range = range(model_mult_logistic_reg_l2, :lambda, lower = 225 , upper = 235, scale = :log),
+                                   range = range(model_mult_logistic_reg_l1, :lambda, lower = 6 , upper = 8, scale = :log),
                                    measure = auc)
 
+                              
+mach_mult_logistic_reg_d_l1 = fit!(machine(tuned_model_mult_logistic_reg_l1, training_dropped_x_std, training_dropped_y))
+pred_mult_logistic_reg_d_l1 = predict_mode(mach_mult_logistic_reg_d_l1, training_dropped_x_std)
+err_rate_mult_logistic_reg_d_l1 = mean(pred_mult_logistic_reg_d_l1 .!= training_dropped_y)
 
-mach_mult_logistic_reg_d_l2 = fit!(machine(tuned_model_mult_logistic_reg_l2, training_dropped_x_std, training_dropped_y))
+print("lambda : ", report(mach_mult_logistic_reg_d_l1).best_history_entry.model.lambda, "\n")
+print("l1 : ", err_rate_mult_logistic_reg_d_l1, "\n")
 
-print("lambda : ", report(mach_mult_logistic_reg_d_l2).best_history_entry.model.lambda, "\n")
+proba_mult_logistic_reg_d_l1 = broadcast(pdf, predict(mach_mult_logistic_reg_d_l1, test_data_std), true)
+prediction_mult_logistic_reg_d_l1_df = DataFrame(id = 1:nrow(test_data_std), precipitation_nextday = proba_mult_logistic_reg_d_l1)
+write_csv("multi_logistic_reg_tuned_l1.csv", prediction_mult_logistic_reg_d_l1_df)
 
-pred_mult_logistic_reg_d_l2 = predict_mode(mach_mult_logistic_reg_d_l2, training_dropped_x_std)
-err_rate_mult_logistic_reg_d_l2 = mean(pred_mult_logistic_reg_d_l2 .!= training_dropped_y)
 
-print("l2 : ", err_rate_mult_logistic_reg_d_l2, "\n")
 
-proba_mult_logistic_reg_d_l2 = broadcast(pdf, predict(mach_mult_logistic_reg_d_l2, test_data_std), true)
-prediction_mult_logistic_reg_d_l2_df = DataFrame(id = 1:nrow(test_data_std), precipitation_nextday = proba_mult_logistic_reg_d_l2)
-write_csv("multi_logistic_reg_tuned_l2.csv", prediction_mult_logistic_reg_d_l2_df)
+# model_mult_logistic_reg_l2 = LogisticClassifier(penalty = :l2)
+
+# tuned_model_mult_logistic_reg_l2 = TunedModel(model = model_mult_logistic_reg_l2,
+#                                    resampling = CV(nfolds = 10),
+#                                    tuning = Grid(),
+#                                    #range = range(model_mult_logistic_reg_l2, :lambda, lower = 3e-2, upper = 3e2, scale = :log),
+#                                    range = range(model_mult_logistic_reg_l2, :lambda, lower = 225 , upper = 235, scale = :log),
+#                                    measure = auc)
+
+
+# mach_mult_logistic_reg_d_l2 = fit!(machine(tuned_model_mult_logistic_reg_l2, training_dropped_x_std, training_dropped_y))
+
+# print("lambda : ", report(mach_mult_logistic_reg_d_l2).best_history_entry.model.lambda, "\n")
+
+# pred_mult_logistic_reg_d_l2 = predict_mode(mach_mult_logistic_reg_d_l2, training_dropped_x_std)
+# err_rate_mult_logistic_reg_d_l2 = mean(pred_mult_logistic_reg_d_l2 .!= training_dropped_y)
+
+# print("l2 : ", err_rate_mult_logistic_reg_d_l2, "\n")
+
+# proba_mult_logistic_reg_d_l2 = broadcast(pdf, predict(mach_mult_logistic_reg_d_l2, test_data_std), true)
+# prediction_mult_logistic_reg_d_l2_df = DataFrame(id = 1:nrow(test_data_std), precipitation_nextday = proba_mult_logistic_reg_d_l2)
+# write_csv("multi_logistic_reg_tuned_l2.csv", prediction_mult_logistic_reg_d_l2_df)
 
 
 
