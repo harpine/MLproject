f41b615af99a659171c1d9ee3f0652f720e6da6a
binkova <helena.binkova@epfl.ch>
2021-12-16 12:44:52 +0100

    save statistics for KNN

--- save_statistics.jl
+++ save_statistics.jl
@@ -62,3 +62,23 @@ function save_statistics_randomForest(machine_subname, tuned_model, machine)
     stats = DataFrame(machine = machine_subname, tuning_parameters = tuning_param, model_report = model_rep, n_trees = tree, max_depth = depth, min_samples_split = min, validation_measure_auc = measure, error_rate = err_rate_randomForest)
     write_stat(machine_subname * ".csv", stats)
 end
+
+function save_statistics_KNN_class(machine_subname, tuned_model, machine, standardized = false)
+    
+    tuning_param = [tuned_model.range]
+
+    model_rep = report(machine).best_history_entry.model
+    K_value = model_rep.K
+
+    measure = report(machine).best_history_entry.measurement
+
+    if standardized
+        pred_KNN_class = predict_mode(machine, training_filled_x_std)
+    else 
+        pred_KNN_class = predict_mode(machine, training_filled_x_std)
+    end
+    err_rate_KNN_class = mean(pred_KNN_class .!= training_filled_y)
+
+    stats = DataFrame(machine = machine_subname, tuning_parameters = tuning_param, model_report = model_rep, K = K_value, validation_measure_auc = measure, error_rate = err_rate_KNN_class)
+    write_stat(machine_subname * ".csv", stats)
+end
