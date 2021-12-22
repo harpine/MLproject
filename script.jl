a83c00b53d313101c8bd657a32ab627096bfd1e9
Harpine <aline.brunner@epfl.ch>
2021-12-22 18:02:02 +0100

    reorganization, script.jl to coordinate

---
+++ script.jl
@@ -0,0 +1,47 @@
+include("./datasets.jl")
+
+function parse_commandline()
+
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--machines"
+            help = "Which machine you want to run. the possibilities are: all, best, logistic_reg, knn, random_forest, short_neuralnetwork or mlp_neuralnetwork. Certain networks are very long to run!"
+            arg_type = String
+            required = true
+            default = "best"
+        "--machine_subname"
+            help = "If you chose to run one specific type of machine, you can specify the string subname of the machine in the second argument. "
+            arg_type = String
+            default = " "
+
+    end
+    return parse_args(s)
+end
+
+function preparation()
+
+    parsed_args = parse_commandline()
+    machines = parsed_args["machines"]
+    machine_subname = parsed_args["machine_subname"]
+
+    # training_data = CSV.read(joinpath(@__DIR__, dataset_folder_name, training_data_name), DataFrame)
+    # test_data = CSV.read(joinpath(@__DIR__, dataset_folder_name, test_data_name), DataFrame)
+
+    # print("Preprocessing data:")
+    # preprocess_data(training_data, test_data)
+
+    include("./multi_logistic_reg.jl")
+    include("./knn_classification.jl")
+    include("./random_forest.jl")
+    include("./short_neuralnetwork.jl")
+    include("./mlp_neuralnetwork.jl")
+    include("./machines_run.jl")
+
+    return machines, machine_subname
+    #run_machines("logistic_reg", " ")
+end
+
+
+machines, machine_subname = preparation()
+run_machines(machines, machine_subname)
